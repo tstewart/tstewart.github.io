4c9717d57dbc69f0c6cdd6fbe5768e6a40ff86fd
Thomas Stewart <tomstewart20@gmail.com>
2023-04-13 10:58:26 +0100

    Add test Leaflet map

---
+++ atheneaum/maptest/geojson.js
@@ -0,0 +1,84 @@
+;(function (window) {
+  // geoJson definitions
+  window.countries = [{
+    type: 'Feature',
+    properties: {
+      name: 'Aelbygg'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [945, 1420]
+    }
+  },
+  {
+    type: 'Feature',
+    properties: {
+      name: 'The Gravelands'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [187, 132]
+    }
+  }]
+
+  window.geoInfo = [{
+    'type': 'Feature',
+    'properties': {
+      'name': 'Burksaal'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [1043, 220]
+    }
+  }, {
+    'type': 'Feature',
+    'properties': {
+      'name': 'Ruins of Elseroth'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [1068, 652]
+    }
+  }, {
+    'type': 'Feature',
+    'properties': {
+      'name': 'Erinmore'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [393, 824]
+    }
+  }, {
+    'type': 'Feature',
+    'properties': {
+      'name': 'Kess'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [656, 907]
+    }
+  },
+  {
+    'type': 'Feature',
+    'properties': {
+      'name': 'Farhome'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [1526, 811]
+    }
+  },
+  {
+    'type': 'Feature',
+    'properties': {
+      'name': 'Daine'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [1414, 1156]
+    }
+  }]
+
+  // polygon
+  window.polygon = []
+}(window))
